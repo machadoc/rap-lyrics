c923c825c646f262c9d015baa1c00e2a08f42994
machadoc <89054402+machadoc@users.noreply.github.com>
2023-12-14 19:31:54 -0500

    Update features.py

--- features.py
+++ features.py
@@ -19,31 +19,8 @@ def profanity_count(lyrics):
     "nigga", "nigger", "fag", "faggot", "cunt", "crap", 
     "asshole", "motherfucker", "piss", "douche"}
 
-    # add prularity for each profane word
-
     return sum(1 for word in lyrics.split() if word in profane_words_set)
 
 def readability_scores(lyrics):
     results = readability.getmeasures(lyrics, lang='en')
     return results['readability grades']['FleschReadingEase']
-
-# def rhyme_count(lyrics):
-#     words = lyrics.split()
-#     rhymes = set()
-    
-#     for word in words:
-#         rhyming_words = pronouncing.rhymes(word)
-#         for rhyme in rhyming_words:
-#             if rhyme in words:
-#                 rhymes.add((word, rhyme))
-    
-#     return len(rhymes)
-
-def literary_devices(lyrics):
-    pass
-
-def thematic_analysis(lyrics):
-    pass
-
-def repetitiveness(lyrics):
-    pass
